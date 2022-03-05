e923cf70ad721899a63c55d546437fdcc1919f69
elady56 <elady02@gmail.com>
2022-03-05 11:36:39 +0100

    Prueba

--- src/main/java/Cyber_Community/Club/Blog.java
+++ src/main/java/Cyber_Community/Club/Blog.java
@@ -1,4 +1,15 @@
 package Cyber_Community.Club;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class Blog {
+
 }
