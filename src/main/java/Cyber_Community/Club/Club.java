790849ddb33994244ec75d280431de6c8b4315cb
elady56 <elady02@gmail.com>
2022-03-05 11:42:55 +0100

    New changes

--- src/main/java/Cyber_Community/Club/Club.java
+++ src/main/java/Cyber_Community/Club/Club.java
@@ -1,4 +1,13 @@
 package Cyber_Community.Club;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Club {
 }
--- src/main/java/Cyber_Community/User/User.java
+++ src/main/java/Cyber_Community/User/User.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
