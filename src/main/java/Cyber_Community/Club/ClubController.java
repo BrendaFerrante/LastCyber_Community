8df957776f54917daae11c8c999f92921bfbac64
elady56 <elady02@gmail.com>
2022-03-05 12:18:25 +0100

    changes in Blog

--- src/main/java/Cyber_Community/Club/ClubController.java
+++ src/main/java/Cyber_Community/Club/ClubController.java
@@ -1,8 +1,23 @@
 package Cyber_Community.Club;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/club")
+@Controller
 public class ClubController {
+    /*@Autowired
+    private ClubRepository clubRepository;*/
 
-
+    @GetMapping("/")
+    public String tablon(Model model) {
+        //model.addAttribute("clubs",clubRepository.);
+        return "Club_template";
+    }
 
 
 }
---
+++ src/main/java/Cyber_Community/Club/ClubRepository.java
@@ -0,0 +1,7 @@
+package Cyber_Community.Club;
+
+/**
+ * Created by Enzo Cotter on 05/03/2022.
+ */
+public interface ClubRepository {
+}
