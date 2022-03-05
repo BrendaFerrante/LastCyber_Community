7c86cc4da805ec36b458f9c2ff321c257323e370
elady56 <elady02@gmail.com>
2022-03-05 14:02:37 +0100

    changes in Club

--- src/main/java/Cyber_Community/Club/ClubController.java
+++ src/main/java/Cyber_Community/Club/ClubController.java
@@ -2,10 +2,20 @@ package Cyber_Community.Club;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ClubController {
     @Autowired
     private ClubRepository clubRepository;
+
+    @GetMapping("/")
+    public String club(Model model) {
+        model.addAttribute("anuncios", clubRepository.findAll());
+        return "Club_template";
+    }
+
 
 }
