68d94d8f433392b53e5b4de8e6b133e47a2026e3
elady56 <elady02@gmail.com>
2022-03-07 22:50:53 +0100

    Elady:changes in Club

--- src/main/java/Cyber_Community/Club/ClubController.java
+++ src/main/java/Cyber_Community/Club/ClubController.java
@@ -2,63 +2,43 @@ package Cyber_Community.Club;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class ClubController {
     @Autowired
     ClubHolder clubHolder;
 
-    @GetMapping("/")
+    @GetMapping("/club")
     public String club(Model model) {
         model.addAttribute("Clubs", clubHolder.getclubs());
-        return "Club_template";
-    }
-    @PostMapping("/Clubs/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Club nuevoClub(@RequestBody Club Club){
-        clubHolder.addClub(Club);
-        return Club;
-    }
-    @DeleteMapping(("/Clubs/{id}"))
-    public ResponseEntity<Club> deleteClub(@PathVariable long id){
-        Club Club=clubHolder.getClub(id);
-        if(Club!=null) {
-            clubHolder.deleteClub(id);
-            return new ResponseEntity<>(Club,HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-    @PutMapping("/Clubs/{id}")
-    public ResponseEntity<Club> putClub(@PathVariable long id,@RequestBody Club Club){
-        Club aux=clubHolder.getClub(id);
-        if(Club!=null) {
-            clubHolder.changeClub(id,Club);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return "IndexClub_template";
     }
 
     @GetMapping("/club/{id}")
-    public ResponseEntity<Club> getClub(@PathVariable long id){
-        Club Club=clubHolder.getClub(id);
-        if(Club!=null) {
-            return new ResponseEntity<>(Club, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public String getClub(Model model,@PathVariable long id){
+        model.addAttribute("club",clubHolder.getClub(id));
+        return "Club_template";
     }
 
 
-
-
+    @PostMapping("/club/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String postClub(Model model,Club club){
+        clubHolder.addClub(club);
+        return "ClubSaved_template";
+    }
+    @DeleteMapping(("/club/{id}"))
+    public String deleteClub(Model model,@PathVariable long id){
+        clubHolder.deleteClub(id);
+        return "ClubDeleted_template";
+    }
+    @PutMapping("/club/{id}")
+    public String putClub(Model model,@PathVariable long id,Club club){
+        clubHolder.changeClub(id,club);
+        return "ClubSaved_template";
+    }
 }
