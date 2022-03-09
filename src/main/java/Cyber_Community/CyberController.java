7cfb27493b205c5a865893cf55b81bfc27e76a7f
elady56 <elady02@gmail.com>
2022-03-09 13:18:33 +0100

    Elady:changes in Club

--- src/main/java/Cyber_Community/CyberController.java
+++ src/main/java/Cyber_Community/CyberController.java
@@ -5,35 +5,34 @@ package Cyber_Community;
 */
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+@Transactional
 public class CyberController {
-
     @GetMapping("/")
-    public String root() {
+    public String root(Model model) {
         return "index";
     }
-
     @GetMapping("/login")
     public String login() {
-        return "index";
+        return "Login.html";
     }
 
     @GetMapping("/login/error")
     public String loginError(Model model) {
         model.addAttribute("loginError","Incorrect username o password");
-        return "index";
+        return "Login.html";
     }
-    @GetMapping("/club")
-    public String club(Model model) {
 
-        return "IndexClub_template";
-    }
 
     @GetMapping("/signup")
     public String signUp() {
-        return "index";
+        return "SignUp.html";
     }
+
 
 }
