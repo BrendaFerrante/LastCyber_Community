f1a13ebc3121d2a9a182311cc4a6716a7c484867
elady56 <elady02@gmail.com>
2022-03-10 16:40:59 +0100

    Elady:changes in Club

--- src/main/java/Cyber_Community/api/exception/apiExceptionController.java
+++
@@ -1,7 +0,0 @@
-package Cyber_Community.api.exception;
-
-/**
- * Created by Enzo Cotter on 09/03/2022.
- */
-public class apiExceptionController {
-}
--- src/main/java/Cyber_Community/web/exception/webExceptionController.java
+++
@@ -1,7 +0,0 @@
-package Cyber_Community.web.exception;
-
-/**
- * Created by Enzo Cotter on 09/03/2022.
- */
-public class webExceptionController {
-}
